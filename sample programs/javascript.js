a015ec37107acdcb8c9c17c3b229367adf2c9ce6
MuhammdJasilk7591 <111067105+MuhammdJasilk7591@users.noreply.github.com>
2023-11-23 16:44:06 +0530

    Add files via upload

---
+++ sample programs/javascript.js
@@ -0,0 +1,29 @@
+
+// function sum(number1,number2,...nums){
+//     let sum=0;
+//     for(let x of nums){
+//         sum=sum+x;
+//     }
+// document.write(number1+number2+sum);
+// }
+// sum(10,20,30,40);
+// ar1=[12,3,4,5,6]
+// ar2=[3,4,67,89,5]
+// ar3=[10,...ar1,...ar2,20,30]
+// document.write(ar3)
+
+
+
+
+// let obj1={
+//     country:'india',
+//     capital:'newdelhi'
+// }
+// let obj2={
+//     language:'hindi'
+// }
+// let obj3={
+//     population:20000
+// }
+// obj4={...obj1,...obj2,...obj3}
+// console.log(obj4)
